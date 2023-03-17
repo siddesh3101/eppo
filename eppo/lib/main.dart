f968bbe24a475c8662a1793c03d349fc102f42a4
Siddesh <siddushetty30@gmail.com>
2023-03-17 19:35:13 +0530

    onBoarding Screen

--- eppo/lib/main.dart
+++ eppo/lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:eppo/screens/home_screen.dart';
+import 'package:eppo/screens/landing_page.dart';
 import 'package:eppo/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +16,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness:
+          Platform.isAndroid ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.grey,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: HomeScreen(),
+      home: LandingPage(),
     );
   }
 }
